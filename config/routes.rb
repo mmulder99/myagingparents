b90573c9916277cda711a1a7a1b7e0e13217af91
michael.mulder <michael.mulder@seekersolutions.com>
2013-06-01 16:02:08 -0700

    Changed the default routing

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,8 @@ Myagingparent::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  match ':id' => 'accounts#show'
+  root :to => 'accounts#new'
 
   # See how all your routes lay out with "rake routes"
 
