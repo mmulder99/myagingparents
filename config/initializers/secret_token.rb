08d24dbc3d1b88af57eebe0e413342445366af93
michael.mulder <michael.mulder@seekersolutions.com>
2013-06-01 11:33:29 -0700

    Initial Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myagingparent::Application.config.secret_token = '30ecb0d001e765f2e707dbcdb997888a9c1c96b870f33820509aa5b4779caf4acdc8e39c477d4686a813fe302b350b4f4d2b4708a2183d4c7ec0656781aa7c55'
